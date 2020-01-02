95f7fb9efd8c90dc5aab8e0cb9406dedffea33ac
Renato <renatomaceneto@gmail.com>
2020-01-02 13:53:40 -0300

    using storage.local to save the urls

--- kipin_extension/src/background.js
+++ kipin_extension/src/background.js
@@ -1,110 +1,145 @@
-// this is like the state of the application
 let URLs = [];
+chrome.storage.local.get(['URLs'], storage => {
+  if (storage.URLs) {
+    URLs = storage.URLs;
+  }
+});
 
-// when the extension is installed or updated
-chrome.runtime.onInstalled.addListener(() => {
-  // first, we need to make the function that will sync the set of urls with the ones there on there server
-  async function syncURLs(token) {
-    const data = await service({
-      url: 'http://localhost:3000/items',
-      method: 'GET',
-      token
-    });
-    const newURLs = data.map(({ body }) => body);
-    URLs = [...URLs, ...newURLs].reduce((acc, value) => {
-      if (!acc.includes(value)) {
-        acc.push(value);
-      }
-      return acc;
-    }, []);
+chrome.storage.sync.get(['token'], async ({ token }) => {
+  if (token) {
+    await syncURLs(token);
+  }
+});
+
+// // when the extension is installed or updated
+// chrome.runtime.onInstalled.addListener(() => {
+
+// });
+
+chrome.webNavigation.onCompleted.addListener(tab => {
+  console.log('ue');
+  console.log(tab);
+  const { tabId, url } = tab;
+  if (URLs.includes(url)) {
+    console.log({ tabId });
+    updateIcon(tabId, true);
+  } else {
+    console.log(url, 'nao pertence a', URLs);
   }
 
-  // second, if the user is authenticated we need to update the URLs
-  chrome.storage.sync.get(['token'], async ({ token }) => {
-    if (token) {
-      await syncURLs(token);
-    }
-  });
+  if (url === 'http://localhost:3000/extension') {
+    chrome.runtime.onMessage.addListener(
+      async (request, sender, sendResponse) => {
+        // if (request.greeting == 'hello') sendResponse({ farewell: 'goodbye' });
+        const { access_token } = request;
 
-  // when then user clicks on extension icon to add a new page
-  chrome.browserAction.onClicked.addListener(tab => {
-    // verify if there is authenticated
-    chrome.storage.sync.get(['token'], async ({ token }) => {
-      // if there is no token it means the user is not logged in
-      if (!token) {
-        // so we go to extension page to get the token
-        chrome.tabs.create({ url: 'http://localhost:3000/extension' });
-      } else {
-        // if the user is authenticated
-        const { title, url } = tab;
-        console.log(token);
-        if (!URLs.includes(url)) {
-          try {
-            const data = await service({
-              url: 'http://localhost:3000/items',
-              method: 'POST',
-              body: { body: url, title },
-              token
-            });
-            console.log(data);
-            // add the new URL to the set
-            URLs.push(data.body);
+        // if the message is to save the token
+        if (access_token) {
+          chrome.storage.sync.set({ token: access_token }, () => {
+            console.log('token set');
+          });
 
-            updateIcon(tab.tabId);
-          } catch (error) {
-            console.log({ error });
-          }
-        } else {
-          updateIcon(tab.tabId);
+          // the user is authenticated lets update the URLs set
+          await syncURLs(access_token);
         }
       }
-    });
-  });
+    );
+  }
+});
 
-  chrome.webNavigation.onCompleted.addListener(({ tabId, url }) => {
-    if (URLs.includes(url)) {
-      updateIcon(tabId);
-    }
+// when then user clicks on extension icon to add a new page
+chrome.browserAction.onClicked.addListener(tab => {
+  // verify if there is authenticated
+  chrome.storage.sync.get(['token'], async ({ token }) => {
+    // if there is no token it means the user is not logged in
+    if (!token) {
+      // so we go to extension page to get the token
+      chrome.tabs.create({ url: 'http://localhost:3000/extension' });
+    } else {
+      console.log(tab);
+      // if the user is authenticated
+      const { title, url, id } = tab;
+      console.log({ title, url, id });
+      console.log({ URLs });
 
-    if (url === 'http://localhost:3000/extension') {
-      chrome.runtime.onMessage.addListener(
-        async (request, sender, sendResponse) => {
-          // if (request.greeting == 'hello') sendResponse({ farewell: 'goodbye' });
-          const { access_token } = request;
-
-          // if the message is to save the token
-          if (access_token) {
-            chrome.storage.sync.set({ token: access_token }, () => {
-              console.log('token set');
-            });
-
-            // the user is authenticated lets update the URLs set
-            await syncURLs(access_token);
-          }
+      if (URLs.includes(url)) {
+        updateIcon(id, true);
+      } else {
+        console.log('pagina nao esta salva');
+        try {
+          const data = await service({
+            url: 'http://localhost:3000/items',
+            method: 'POST',
+            data: { body: url, title },
+            token
+          });
+          console.log('depois de salvar', data);
+          // add the new URL to the set
+          //URLs.push(data.body);
+          updateIcon(id, true);
+          await syncURLs(token);
+        } catch (error) {
+          console.log('erro ao salvar a pagina', error);
         }
-      );
+      }
     }
   });
 });
 
+async function syncURLs(token) {
+  const data = await service({
+    url: 'http://localhost:3000/items',
+    method: 'GET',
+    token
+  });
+  console.log(data);
+  const newURLs = data.map(({ body }) => body);
+
+  chrome.storage.local.set({ URLs: newURLs }, () => {
+    console.log('URLs updated');
+    URLs = newURLs;
+  });
+
+  // URLs = [...URLs, ...newURLs].reduce((acc, value) => {
+  //   if (!acc.includes(value)) {
+  //     acc.push(value);
+  //   }
+  //   return acc;
+  // }, []);
+}
+
 // this is a helper for the fetch function
-async function service({ url, method, body, token }) {
+async function service({ url, method, data, token }) {
   const headers = new Headers();
   headers.append('Content-Type', 'application/json');
   headers.append('Authorization', `Bearer ${token}`);
 
+  let body;
+  console.log(method, url);
+  if (method === 'POST') {
+    body = JSON.stringify(data);
+    console.log(body);
+  }
+
   const requestConfig = {
     method: method,
     headers,
-    body: method === 'GET' ? undefined : JSON.stringify(body)
+    body
   };
 
-  const response = await fetch(url, requestConfig);
-  return response.json();
+  try {
+    const response = await fetch(url, requestConfig);
+    return response.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
 
-function updateIcon(tabId) {
-  chrome.browserAction.setIcon({ path: 'icon.png', tabId }, () => {
+function updateIcon(tabId, saved) {
+  const path = saved === true ? 'icon.png' : 'icon_2.png';
+  console.log({ path });
+  chrome.browserAction.setIcon({ path, tabId }, () => {
     chrome.browserAction.setTitle({
       title: 'This page is already save',
       tabId
