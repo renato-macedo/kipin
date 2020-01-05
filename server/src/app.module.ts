238e733d0812f062c340b3ea44e00a2481a61526
Renato <renatomaceneto@gmail.com>
2020-01-04 23:31:07 -0300

    setting up SPA

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemModule } from './item/item.module';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -17,6 +18,9 @@ import { AuthModule } from './auth/auth.module';
     }),
     UserModule,
     AuthModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'website', 'dist'),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ website/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles/index.css';
+
+function App(): JSX.Element {
+  return <h1>Hello World</h1>;
+}
+
+export default App;
